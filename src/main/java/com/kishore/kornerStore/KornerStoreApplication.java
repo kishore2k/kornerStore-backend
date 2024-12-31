2b6de93255af0a536733ea1a855e6094868cc430
kishore2k <contactkishore2000@gmail.com>
2024-12-31 23:04:20 +0530

    initialSetupFiles

---
+++ src/main/java/com/kishore/kornerStore/KornerStoreApplication.java
@@ -0,0 +1,13 @@
+package com.kishore.kornerStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KornerStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KornerStoreApplication.class, args);
+	}
+
+}
