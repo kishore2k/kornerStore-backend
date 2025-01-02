3e435086783ba91a8bf64bfbc29fff94562b8942
kishore2k <contactkishore2000@gmail.com>
2025-01-02 22:11:23 +0530

    getProducts

---
+++ src/main/java/com/kishore/kornerStore/model/Product.java
@@ -0,0 +1,28 @@
+package com.kishore.kornerStore.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "product")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private String category;
+    private BigDecimal price;
+    private Date releaseDate;
+    private boolean available;
+    private int qty;
+}
